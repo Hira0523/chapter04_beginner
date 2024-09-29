35f6a3b7a5f3f358dd16c0120d74cd5ef7a24047
Hira0523 <hiranosya0523.uw@gmail.com>
2024-09-29 19:32:28 +0900

    Row実装

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
+      body: Row(
         crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
           Container(
